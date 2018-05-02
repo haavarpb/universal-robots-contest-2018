2ee764b462aa437981c9613fee5a903de7add80a
haavarpb <haavard.brandal@gmail.com>
2018-05-02 17:41:18 +0200

    Added a simple TCP Server

--- tcpListener.py
+++ tcpListener.py
@@ -1,1 +1,11 @@
 import socket
+
+TCP_IP = '127.0.0.1'
+TCP_PORT = 23
+BUFFER_SIZE = 1024
+MESSAGE = "(0)"+"\n"
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((TCP_IP, TCP_PORT))
+s.send(MESSAGE)
+data = s.recv(BUFFER_SIZE)
+s.close()
