fde0b4b76f86cfaf42707625e3ae88646f6c6c34
haavarpb <haavard.brandal@gmail.com>
2018-05-03 11:06:15 +0200

    NLA

--- URServer.py
+++ URServer.py
@@ -10,7 +10,7 @@ class URServer:
         print "Initiating server..."
         self._ur1_qi = Queue.Queue()
         self._ur1_qo = Queue.Queue()
-        self._ur1_port = 1111
+        self._ur1_port = 2222
         self._ur1_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self._ur1_socket.bind(("", self._ur1_port))
         self._ur1_thread = threading.Thread(target = self._serve, name = "UR1 server", args=(self._ur1_qi, self._ur1_qo, self._ur1_socket))
@@ -28,22 +28,25 @@ class URServer:
         """ Mailman function """
         socket.listen(1)
         client_socket, addr = socket.accept()
-
         while True:
-			payload = q_out.get()
-			print "Payload ready " + payload
-			client_socket.send(payload)
-			data = client_socket.recv(1024)
-			q_in.put(data)
+            while q_out.empty():
+                time.sleep(0.5)
+            payload = q_out.get_nowait()
+            print "Payload ready ",payload
+            client_socket.send(payload)
+            print "Payload sent"
+            data = client_socket.recv(1024)
+            q_in.put(data)
 
 	def getMessage(self, target):
 		""" Get message from URX X = [1, 2] if any. None if none """
-        try:
-            if target == "UR1":
-                return self._ur1_qi.get(block=False)
-            #elif target == "UR2":
-                # return self._ur2_qi.get(block=False)
-        except Queue.Empty:
+        if target == "UR1":
+            if not self._ur1_qi.empty():
+                return self._ur1_qi.get_nowait()
+        #elif target == "UR2":
+            #if not self._ur2_qi.empty()
+                # return self._ur2_qi.get_nowait()
+        else:
             return None
 
     def sendMessage(self, target, message):
@@ -52,9 +55,3 @@ class URServer:
 			self._ur1_qo.put(message)
         #elif target == "UR2":
             # self._ur2_qo.put(message)
-
-	def shutdown(self):
-		self._ur1_socket.shutdown()
-		self._ur1_socket.close()
-		# self._ur2_socket.shutdown()
-		# self._ur2_socket.close()
