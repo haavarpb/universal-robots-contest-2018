8f4dfc74f3f099feefd16971319dc5a04deecc44
javigallostra <jgallostra@gmail.com>
2018-05-03 10:15:55 +0200

    hola

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#from Camera import Camera
+from Camera import Camera
 from BTServer import BTServer
 from URServer import URServer
 
@@ -14,7 +14,7 @@ R2_MOVING = 5
 #########
 
 # Main objects
-CAM = 0#Camera()
+CAM = Camera()
 BTS = BTServer()
 URS = URServer()
 
