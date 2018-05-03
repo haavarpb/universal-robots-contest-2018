548a52da23d17c8209e3aa21e988effa17453907
haavarpb <haavard.brandal@gmail.com>
2018-05-03 12:00:45 +0200

    Updating server

--- URServer.py
+++ URServer.py
@@ -6,38 +6,40 @@ import time
 class URServer:
     """ Server program enabling connection to the two UR robots """
 
-    def __init__(self):
+    def __init__(self, port):
         self.qi = Queue.Queue()
         self.qo = Queue.Queue()
-        self.port = 2222
+        self.port = port
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.bind(("", self.port))
         self.thread_listen = threading.Thread(target = self._listen)
         self.thread_listen.start()
+        self.thread_send = threading.Thread(target = self._send)
+        self.thread_send.start()
 
-
-    def _serve(self):
+    def _listen(self):
         """ Mailman function """
         self.s.listen(1)
         client_socket, addr = socket.accept()
         while True:
-            if not q_out.empty():
-                payload = q_out.get_nowait()
-                print "Payload ready ",payload
-                client_socket.send(payload)
-                print "Payload sent"
             data = client_socket.recv(1024)
-            print data
+            print "Got message: ", data
             q_in.put(data)
+
+    def _send(self):
+        """ Send messages as long as it is available """
+        while True:
+            if not self.qo.empty():
+                payload = self.qo.get_nowait()
+                self.socket.send(payload)
 
 	def getMessage(self, target):
 		""" Get message from URX X = [1, 2] if any. None if none """
-        if not self._ur1_qi.empty():
-            return self._ur1_qi.get_nowait()
+        if not self.qi.empty():
+            return self.qi.get_nowait()
         else:
             return None
 
     def sendMessage(self, target, message):
         """ Send message to target UR """
-		self._ur1_qo.put(message)
-
+		self.qo.put(message)
